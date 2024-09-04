06e405c4e93f23a638946a5952fb5939c5da2e26
b-smith94 <byrondsmith9@gmail.com>
2024-09-04 12:08:20 -0400

    Reorganized functions into WeatherDataFetcher and the subclass DataParser, and edited the code so that it functioned within the class and subclass.

--- weather_forcast.py
+++ weather_forcast.py
@@ -1,28 +1,36 @@
 #Task 1
-def fetch_weather_data(city):
-    print(f"Fetching weather data for {city}...")
-    weather_data = {
-            "New York": {"temperature": 70, "condition": "Sunny", "humidity": 50, "city": "New York"},
-            "London": {"temperature": 60, "condition": "Cloudy", "humidity": 65, "city": "London"},
-            "Tokyo": {"temperature": 75, "condition": "Rainy", "humidity": 70, "city": "Tokyo"}
-        }
-    return weather_data.get(city, {})
+class WeatherDataFetcher:
+    def __init__(self, city):
+        self.city = city
 
-def parse_weather_data(data):
-    if not data:
-        return "Weather data not available"
-    city = data["city"]
-    temperature = data["temperature"]
-    condition = data["condition"]
-    humidity = data["humidity"]
-    return f"Weather in {city}: {temperature} degrees, {condition}, Humidity: {humidity}%"
+    def fetch_weather_data(city):
+        print(f"Fetching weather data for {city}...")
+        weather_data = {
+                "New York": {"temperature": 70, "condition": "Sunny", "humidity": 50, "city": "New York"},
+                "London": {"temperature": 60, "condition": "Cloudy", "humidity": 65, "city": "London"},
+                "Tokyo": {"temperature": 75, "condition": "Rainy", "humidity": 70, "city": "Tokyo"}
+            }
+        return weather_data.get(city, {})
 
-def display_weather(city):
-    data = fetch_weather_data(city)
-    if not data:
-        print(f"Weather data not available for {city}")
-    else:
-        return parse_weather_data(data)
+class DataParser(WeatherDataFetcher):
+    def __init__(self, city):
+        super().__init__(city)
+
+    def parse_weather_data(weather_data):
+        if not weather_data:
+            return "Weather data not available"
+        city = weather_data["city"]
+        temperature = weather_data["temperature"]
+        condition = weather_data["condition"]
+        humidity = weather_data["humidity"]
+        return f"Weather in {city}: {temperature} degrees, {condition}, Humidity: {humidity}%"
+
+    def display_weather(city):
+        data = WeatherDataFetcher.fetch_weather_data(city)
+        if not data:
+            print(f"Weather data not available for {city}")
+        else:
+            return DataParser.parse_weather_data(data)
 
 def main():
     while True:
@@ -30,7 +38,7 @@ def main():
         if city == 'Exit':
             break
         else:
-            forecast = display_weather(city)
+            forecast = DataParser.display_weather(city)
         print(forecast)
 
 if __name__ == "__main__":
